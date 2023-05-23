db27595dee7ac768cfa25fc63b626b4c6befeb8e
johannesnoordanus <52376919+johannesnoordanus@users.noreply.github.com>
2023-05-23 17:23:32 +0200

    Fix of gcode emit for same pixel values (at end of sweep line)

--- image2gcode.py
+++ image2gcode.py
@@ -2,15 +2,20 @@
 """
 image2gcode: convert an image to gcode.
 """
-__version__ = "v1.0.0-beta"
+__version__ = "v1.1.0-beta"
 
 import sys
-import re
 import argparse
-from typing import TextIO
+from argparse import Namespace
 from datetime import datetime
 from PIL import Image
 import numpy as np
+
+GCODE2IMAGE = True
+try:
+    from gcode2image import gcode2image
+except ImportError:
+    GCODE2IMAGE = False
 
 def loadImage(imagefile: str = None, showimage: bool = True):
     """
@@ -35,103 +40,6 @@ def loadImage(imagefile: str = None, showimage: bool = True):
 
     return None
 
-def gcode2image(gcode: TextIO, dimensions, args) -> np.array:
-    """
-    Convert gcode (file) to image.
-    """
-    iheight, iwidth = dimensions
-    S_max = args.maxpower
-
-    invert_intensity = True
-
-    def pixel_intensity(S: int = None) -> int:
-        # pixel intensity (inverse) proportional to laser power
-        return round((1.0 - float(S/S_max)) * 255) if invert_intensity else round(float(S/S_max) * 255)
-
-    def set_coordinates(X: int = None, Y: int = None, S: int = None):
-        nonlocal x
-        nonlocal y
-        if X is not None :
-            if x == X:
-                # this should not happen!
-                #raise ValueError(f"Linear move should not go to the same location on a line: ({x},{y}) -> ({X},{y})")
-                print(f"Linear move should not go to the same location on a line: ({x},{y}) -> ({X},{y})")
-                return
-
-            # Note that (laser) drawing from a point to a point differs from setting a point (within an image):
-            #              0   1   2   3   4   5   6     <-- gcode drawing points
-            # one line:    |[0]|[1]|[2]|[3]|[4]|[5]|     <-- image pixels
-            #
-            # For example: drawing from X0 to X2 with value S10 corresponds to setting [0] = S10 and [1] = S10
-            # Note also that drawing form left to right differs subtly from the reverse
-
-            # set point intensity value
-            if x < X:
-                # draw from left to right, note the range (interval): [x,X)
-                for x_coord in range(x,X):
-                    image[y - start_Y, x_coord - start_X] = pixel_intensity(S)
-                    #print("[", x_coord - start_X, ",", y - start_Y, "] = ", "(", S, ")", pixel_intensity(S))
-            else:
-                # draw from right to left, note the range (interval): [X,x)
-                for x_coord in range(X,x):
-                    image[y - start_Y, x_coord - start_X] = pixel_intensity(S)
-                    #print("[", x_coord - start_X, ",", y - start_Y, "] = ", "(", S, ")", pixel_intensity(S))
-            # set current x
-            x = X
-        if Y is not None:
-            # go to next line
-            #y = Y-start_Y
-            y = Y
-
-    def XY() -> str:
-        nonlocal gcode
-        nonlocal last_S
-        # find next XY-goto coordinate line
-        line = gcode.readline()
-        if line != '' and not 'G0' in line:
-            if "X" in line or "Y" in line:
-                # get X (and S) or get Y (until /n)
-                X = re.search("X[0-9]+\.[0-9]+",line)
-                Y = re.search("Y[0-9]+\.[0-9]+",line)
-                S = re.search("S[0-9]+",line)
-                if S:
-                    S = int(S.group(0)[1:])
-                    last_S = S
-                else:
-                    S = last_S
-                if X:
-                    # get X coordinate,
-                    # get S,
-                    X = round(float(X.group(0)[1:])/args.pixelsize)
-                    set_coordinates(X = X, S = S)
-                if Y:
-                    Y = round(float(Y.group(0)[1:])/args.pixelsize)
-                    set_coordinates(Y = Y)
-        return line
-    #
-    # setup/init
-
-    # create image array (hight,width) with unsigned 8 bit elements
-    image = np.full([iheight, iwidth], 255, dtype=np.uint8)
-
-    # gcode modes stay untill changed
-    last_S = None
-
-    # offset (X,Y)
-    start_X = round(float(args.offset[0])/args.pixelsize)
-    start_Y = round(float(args.offset[1])/args.pixelsize)
-    # current (x,y)
-    x = start_X
-    y = start_Y
-
-    # repeat until EOF
-    #     find next XY coordinate and set corresponding image pixel(s) (inverse) intensity
-    line = XY()
-    while line != '':
-        line = XY()
-
-    return image
-
 def image2gcode(img, args) -> str:
     """
     image2gcode: convert image to gcode
@@ -153,9 +61,9 @@ def image2gcode(img, args) -> str:
     gcode_footer = ["M5","M9","M2"]
 
     # header comment
-    gcode = [';\n;    ' + sys.argv[0] + " (" + str(datetime.now()).split('.')[0] + ")\n" +
-        ';    Print area: ' + str(img.shape[1] * args.pixelsize) + "mm x " + str(img.shape[0] * args.pixelsize) + "mm (XY)\n" +
-        ';    Settings: pixelsize ' + str(args.pixelsize) + ' mm^2, speed ' + str(args.speed) + ' mm/min, maxpower ' + str(args.maxpower) + ', offset ' + str(args.offset) + "\n;\n" ]
+    gcode = [';\n;    ' + sys.argv[0] + " " +  __version__ + " (" + str(datetime.now()).split('.')[0] + ")\n" +
+        ';    Area: ' + str(round(img.shape[1] * args.pixelsize,2)) + "mm x " + str(round(img.shape[0] * args.pixelsize,2)) + "mm (XY)" +
+        ';    > pixelsize ' + str(args.pixelsize) + ' mm^2, speed ' + str(args.speed) + ' mm/min, maxpower ' + str(args.maxpower) + ', offset ' + str(args.offset) + "\n;\n" ]
 
     # init gcode
     gcode += ["G00 G17 G40 G21 G54","G90"]
@@ -167,12 +75,12 @@ def image2gcode(img, args) -> str:
     Y = round(args.offset[1], XY_prec)
 
     # go to start
-    gcode += [f"G0 X{X} Y{Y}"]
+    gcode += [f"G0X{X}Y{Y}"]
 
     # set write speed and G1 move mode
     # (note that this stays into effect until another G code is executed,
     #  so we do not have to repeat this for all coordinates emitted below)
-    gcode += [f"G1 F{args.speed}"]
+    gcode += [f"G1F{args.speed}"]
 
     # Print left to right, right to left (etc.)
     # Optimized gcode:
@@ -190,7 +98,7 @@ def image2gcode(img, args) -> str:
             # reverse line when printing right to left
             line = np.flip(line)
 
-        # add line terminator (to make the
+        # add line terminator (makes this algorithm regular)
         line = np.append(line,0)
 
         # Note that (laser) drawing from a point to a point differs from setting a point (within an image):
@@ -202,7 +110,6 @@ def image2gcode(img, args) -> str:
 
         # draw this pixel line
         for count, pixel in enumerate(line):
-            # power proportional to maximum laser power
             laserpow = round((1.0 - float(pixel/255)) * args.maxpower) if invert_intensity else round(float(pixel/255) * args.maxpower)
 
             # delay emit first pixel (so all same power pixels can be emitted in one sweep)
@@ -212,9 +119,8 @@ def image2gcode(img, args) -> str:
             # draw pixels until change of power
             if laserpow != prev_pow or count == line.size-1:
                 code = f"X{X}"
-                code += f"S{prev_pow}" if laserpow != prev_pow else ''
+                code += f"S{prev_pow}"
                 gcode += [code]
-
                 if count == line.size-1:
                     continue
                 prev_pow = laserpow # if laserpow != prev_pow
@@ -261,11 +167,19 @@ def main() -> int:
 
     args = parser.parse_args()
 
+    if args.validate and (args.gcode.name == "<stdout>" or not GCODE2IMAGE):
+        if not GCODE2IMAGE:
+            print("For validation package gcode2image.py must be installed (in $PATH or python install path)")
+        else:
+            print("For validation a 'gcode filename' argument must be set.")
+            parser.print_usage(sys.stderr)
+        sys.exit(1)
+
     # load and convert image to B&W
     narr = loadImage(args.image.name,args.showimage)
 
-    print('Print area: ' + str(narr.shape[1] * args.pixelsize) + "mm x " + str(narr.shape[0] * args.pixelsize) + "mm (XY)")
-    print('Settings: pixelsize', args.pixelsize, 'mm^2, speed', args.speed, 'mm/min, maxpower ' + str(args.maxpower) + ', offset', args.offset)
+    print('Area: ' + str(round(narr.shape[1] * args.pixelsize,2)) + "mm x " + str(round(narr.shape[0] * args.pixelsize,2)) + "mm (XY)")
+    print('> pixelsize', args.pixelsize, 'mm^2, speed', args.speed, 'mm/min, maxpower ' + str(args.maxpower) + ', offset', args.offset)
 
     # emit gcode for image
     print(image2gcode(narr, args), file=args.gcode)
@@ -273,15 +187,16 @@ def main() -> int:
     if args.validate:
         args.gcode.close()
         with open(args.gcode.name, "r") as fgcode:
-            img = gcode2image(fgcode, narr.shape, args)
+            #img = gcode2image(fgcode, narr.shape, args)
+            img = gcode2image(Namespace(gcode = fgcode, offset = False, showG0 = True))
 
             # convert to image
             img = Image.fromarray(img)
 
             # show image
             img.show()
-            # write image file (jpeg)
-            #pic.save(args.gcode.name.split('.')[0] + '_' + sys.argv[0].split('.')[0] + '.jpg')
+            # write image file (png)
+            #pic.save(args.gcode.name.split('.')[0] + '_' + sys.argv[0].split('.')[0] + '.png')
 
     return 0
 
